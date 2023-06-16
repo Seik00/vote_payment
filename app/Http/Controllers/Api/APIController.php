4c8cf1e40902b3e840c0fbdc56f9fce89e26e157
Seik00 <seikyusiang@gmail.com>
2023-06-16 16:34:25 +0800

    a

--- app/Http/Controllers/Api/APIController.php
+++ app/Http/Controllers/Api/APIController.php
@@ -1,13 +1,8 @@
 <?php
 namespace App\Http\Controllers\Api;
 
-use App\Helper\Bonus;
 use App\Http\Controllers\Controller;
-use App\Models\GlobalSetting;
-use App\Models\Product;
-use App\Models\ProductPurchase;
-use App\Models\ProductRedeem;
-use App\Models\SystemBank;
+use App\Models\UserOtpCode;
 use App\User;
 use DB;
 use Illuminate\Http\Request;
@@ -25,304 +20,38 @@ class APIController extends Controller
             <br><br>
             Kind regards.<br> Management<br><br><br>Note : System generated email, please do not reply this email.');
         exit;
-        //return $user->checkDownline(1000045,'j');
-        //DB::select('call sendBV("' . $user->id . '","' . (int)$user->package->bv . '")');
-        // $r = Bonus::special_bonus();
-        // $r = Bonus::single_static_bonus(1000007);
-        // echo $r;
+       
     }
-    /**
-     * @OA\GET(
-     *     path="/api/project/lookup",
-     *     tags={"Project"},
-     *     summary="get system setting",
-     *     description="get notice,system setting",
-     *     operationId="lookup",
-     *     @OA\Response(
-     *         response=200,
-     *         description="successful operation"
-     *     ),
-     *     @OA\Response(
-     *         response=422,
-     *         description="Error"
-     *     )
-     * )
-     */
-    public function lookup(Request $request)
+    public function requestEmailOtp(Request $request)
     {
-        $global = GlobalSetting::get()->toArray();
+        $this->validate($request, [
+            'email' => 'required',
 
-        foreach ($global as $x => $val) {
-            $system[$val['global_key']] = $val['key_value'];
+        ]);
+        $code = mt_rand(100000, 999999);
+   
+        $r = UserOtpCode::updateOrCreate([
+            'email' => $request->get('email'),
+        ], [
+            'code' => $code,
+            'expiry_date' => now()->addMinutes(5),
+        ]);
+        $email = $request->get('email');
+        if($r){
+            $this->smtp_mail($email, 'Please verify your code', '<h4>Verification Code</h4> <br/> <h1> '. $code.' </h1> <br/><br/> 
+            Username :123
+            <br>Password :321
+            <br><a href="https://fizz.greatwallsolution.com/">https://fizz.greatwallsolution.com/</a>
+                <br><br>
+                Kind regards.<br> Management<br><br><br>Note : System generated email, please do not reply this email.');
         }
 
-        return $this->jsonResponse([
+        return response()->json([
             'code' => 0,
-            'data' => [
-                'system' => $system,
-            ],
-            'message' => '',
-        ]);
-    }
-    /**
-     * @OA\GET(
-     *     path="/api/project/productList",
-     *     tags={"Project"},
-     *     summary="get product list",
-     *     description="get notice,system setting",
-     *     operationId="productList",
-     *      security={{"bearerAuth":{}}},
-     *     @OA\Response(
-     *         response=200,
-     *         description="successful operation"
-     *     ),
-     *     @OA\Response(
-     *         response=422,
-     *         description="Error"
-     *     )
-     * )
-     */
-    public function productList(Request $request)
-    {
-        $user = auth()->user();
-
-        $product = Product::where('is_display', 1)->get();
-
-        return $this->jsonResponse([
-            'code' => 0,
-            'data' => [
-                'product' => $product,
-            ],
-            'message' => '',
-        ]);
-    }
-    /**
-     * @OA\Post(
-     *     path="/api/project/purchaseProduct",
-     *     tags={"Project"},
-     *     summary="purchaseProduct",
-     *     description="Register member",
-     *     operationId="submitShareRecord",
-     *      security={{"bearerAuth":{}}},
-     *     deprecated=false,
-     *  *     @OA\Parameter(
-     *         name="sec_password",
-     *         in="query",
-     *         description="sec_password",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="string"
-     *         )
-     *     ),
-     *     @OA\Parameter(
-     *         name="product_id",
-     *         in="query",
-     *         description="product_id",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="string"
-     *         )
-     *     ),
-     *      @OA\Parameter(
-     *         name="pay_type",
-     *         in="query",
-     *         description="pay_type",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="string"
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="successful operation"
-     *     ),
-     *     @OA\Response(
-     *         response=422,
-     *         description="Error"
-     *     )
-     * )
-     */
-    public function purchaseProduct(Request $request)
-    {
-        $user = auth()->user();
-        $error = $this->check_secpass($request->get('sec_password'));
-        if ($error) {
-            return $error;
-        }
-        $product = Product::where('id', $request->get('product_id'))->where('is_display', 1)->first();
-        if (!$product) {
-            return $this->jsonResponse([
-                'code' => 2,
-                'data' => false,
-                'message' => 'Incorrect Product',
-            ]);
-        }
-        $pay = $request->get('quantity') * $product->price;
-
-        if ($request->get('pay_type') == 'point3') {
-            $wallet = 'point3';
-            $wallet_type = 3;
-            $pay1 = $pay;
-            $wallet2 = 'point2';
-            $wallet_type2 = 2;
-            $pay2 = 0;
-
-        } elseif ($request->get('pay_type') == 'point3&pin') {
-            $wallet = 'point3';
-            $wallet_type = 3;
-            $pay1 = $pay * 0.5;
-            $wallet2 = 'pin';
-            $wallet_type2 = 10;
-            $pay2 = $pay * 0.5;
-
-        } else {
-            $error = 'INSUFFICIAN_BALANCE';
-        }
-
-        if ($user->$wallet < $pay1 || $user->$wallet2 < $pay2) {
-            $error = 'INSUFFICIAN_BALANCE';
-        }
-
-        if ($error) {
-            return $this->jsonResponse([
-                'code' => 2,
-                'data' => false,
-                'message' => $error,
-            ]);
-        }
-        $r = $this->create_transaction($user->id, '-', $wallet, 10, $wallet_type, $pay1, '购买产品' . $product->product_name, 'Repurchase Of ' . $product->product_name_en . '  Product = ' . $request->get('quantity') . ' Bottle', 'Beli paket ' . $product->product_name_en . ' dengan kuantiti ' . $request->get('quantity'), );
-        if ($pay2 > 0) {
-            $this->create_transaction($user->id, '-', $wallet2, 11, $wallet_type2, $pay2, '购买产品' . $product->product_name, 'Repurchase Of ' . $product->product_name_en . ' Product = ' . $request->get('quantity') . ' Bottle', 'Beli paket ' . $product->product_name_en . ' dengan kuantiti ' . $request->get('quantity'), );
-        }
-        ProductRedeem::create([
-            'user_id' => $user->id,
-            'user_package_id' => 0,
-            'product_id' => $product->id,
-            'quantity' => $request->get('quantity'),
-        ]);
-        $purchase = ProductPurchase::create([
-            'user_id' => $user->id,
-            'pv' => $request->get('quantity') * $product->pv,
-            'product_id' => $product->id,
-            'quantity' => $request->get('quantity'),
+            'data' => $code,
+            'message' => 'OTP_sent',
         ]);
 
-        DB::select('call sendBV("' . $user->id . '","' . $purchase->pv . '")');
-        $user->today_purchase = 1;
-        $user->save();
-        // - $currect->price;
-        /* if ($request->get('pay_type') == 'point2') {
-
-        } else {
-
-        }*/
-
-        if ($r) {
-            return $this->jsonResponse([
-                'code' => 0,
-                'data' => true,
-                'message' => 'REQUEST_COMPLETE',
-            ]);
-        } else {
-            return $this->jsonResponse([
-                'code' => 1,
-                'data' => false,
-                'message' => 'REQUEST_FAIL',
-            ]);
-        }
-    }
-    /**
-     * @OA\GET(
-     *     path="/api/project/get-share-record",
-     *     tags={"Project"},
-     *     summary="getShareRecord",
-     *     description="Register member",
-     *     operationId="getShareRecord",
-     *      security={{"bearerAuth":{}}},
-
-     *     @OA\Response(
-     *         response=200,
-     *         description="successful operation"
-     *     ),
-     *     @OA\Response(
-     *         response=422,
-     *         description="Error"
-     *     )
-     * )
-     */
-
-    public function getProductRedeem(Request $request)
-    {
-
-        $user = auth()->user();
-
-        $record = ProductRedeem::where('user_id', $user->id)->orderBy('id', 'desc')->paginate(10);
-
-        return $this->jsonResponse([
-            'code' => 0,
-            'data' => $record,
-            'message' => '',
-        ]);
-    }
-
-    public function getShareRecord(Request $request)
-    {
-
-        $user = auth()->user();
-
-        $record = ProjectShare::where('user_id', $user->id)->orderBy('id', 'desc')->paginate(10);
-
-        return $this->jsonResponse([
-            'code' => 0,
-            'data' => $record,
-            'message' => '',
-        ]);
-    }
-
-    /**
-     * @OA\GET(
-     *     path="/api/project/get-deposit-bank",
-     *     tags={"Project"},
-     *     summary="getDepositBank",
-     *     description="get deposit bank",
-     *     operationId="getDepositBank",
-     *      security={{"bearerAuth":{}}},
-     *     @OA\Parameter(
-     *         name="country_id",
-     *         in="query",
-     *         description="country_id",
-     *         required=false,
-     *         @OA\Schema(
-     *             type="string"
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="successful operation"
-     *     ),
-     *     @OA\Response(
-     *         response=422,
-     *         description="Error"
-     *     )
-     * )
-     */
-    public function getDepositBank(Request $request)
-    {
-
-        $user = auth()->user();
-        if ($request->get('country_id')) {
-            $record = SystemBank::where('is_display', 1)->where('bank_country', $request->get('country_id'))->orderBy('id', 'desc')->get();
-        } else {
-            $record = SystemBank::where('is_display', 1)->orderBy('id', 'desc')->get();
-        }
-
-        return $this->jsonResponse([
-            'code' => 0,
-            'data' => [
-                'system_bank' => $record,
-            ],
-            'message' => '',
-        ]);
     }
 
 }

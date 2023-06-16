f70cb068e6d84d599296d64efff53812337f2145
WChienMing <woochienming@gmail.com>
2023-06-16 15:35:28 +0800

    order models

---
+++ app/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'payment_gateway_order';
+
+    protected $fillable = [
+        'usdt_address',
+        'username',
+        'email',
+        'currency',
+        'order_no', 
+        'merchant_code',
+        'from_bank',
+        'bank_account_number',
+        'amount',
+        'trans_id',
+        'payment_status',
+        'payment_done',
+        'error_message',
+        'created_at',
+        'updated_at'
+    ];
+}

b4bcb535604ce36d05398cc9b3fc73ca45fd4705
Seik00 <seikyusiang@gmail.com>
2023-06-16 13:15:49 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ Route::group(['prefix' => '/'], function () {
     Route::get('/', function () {
         return redirect()->route('loginPage');
     });
- 
-    Route::group(['namespace' => 'home'], function () {
-        Route::get('/watch/{video_name}', 'HomeController@index')->name('watch');
-    });
 
     Route::group(['namespace' => 'web'], function () {
         Route::get('/login', 'SiteController@loginPage')->name('site_index');
@@ -67,56 +63,3 @@ Route::group(['prefix' => 'web', 'namespace' => 'web'], function () {
     });
 });
 
-Route::group(['prefix' => 'admin'], function () {
-    // Voyager::routes(); //override
-    // Route::Auth();
-    Route::get('/', 'LoginController@loginPage')->name('login');
-    Route::get('/login', 'LoginController@loginPage')->name('login');
-    Route::get('/logout', 'LoginController@Logout')->name('logout');
-    Route::post('/validate_login', 'LoginController@validate_login')->name('validate_login');
-    Route::get('/forgot-password', 'Auth\ForgotPasswordController@index')->name('user_forgot_password');
-    Route::post('/reset-password', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password_email');
-
-    Route::group(['namespace' => 'admin'], function () {
-        Route::post('/do_setting', 'HomeController@do_setting')->name('index');
-        Route::post('/do_add_users', 'HomeController@do_add_users')->name('index');
-        Route::post('/table_action', 'HomeController@table_action')->name('index');
-        Route::get('/getData', 'HomeController@getData')->name('index');
-        Route::group(['middleware' => ['auth', 'role:admin']], function () {
-
-            Route::group(['prefix' => 'home'], function () {
-                Route::get('/', 'HomeController@index')->name('index_home');
-            });
-
-            Route::group(['prefix' => 'setting'], function () {
-                Route::get('/', 'HomeController@setting')->name('setting_home');
-            });
-
-            Route::group(['prefix' => 'user'], function () {
-                Route::get('/user_list', 'UserController@user_list')->name('user_list');
-                Route::post('/set_blacklist', 'UserController@set_blacklist');
-                Route::post('/delete_admin', 'UserController@delete_admin');
-                Route::get('/admin_list', 'UserController@admin_list')->name('admin_list');
-                Route::post('/update_admin', 'UserController@update_admin');
-                Route::post('/create_admin', 'UserController@create_admin');
-                Route::post('/get_user_info', 'UserController@get_user_info')->name('get_user_info');
-                Route::get('/userPackage', 'UserController@userPackage')->name('userPackage');
-                Route::post('/updateUser', 'UserController@updateUser');
-                Route::get('/userAddress', 'UserController@userAddress')->name('userAddress');
-
-                Route::get('/setUserPwd', 'UserController@setUserPwd')->name('setUserPwd');
-                Route::post('/updatePwd', 'UserController@updatePwd')->name('updatePwd');
-            });
-
-            Route::group(['prefix' => 'setting'], function () {
-                Route::get('/systemConfig', 'SettingController@systemConfig')->name('systemConfig');
-                Route::post('/saveConfig', 'SettingController@saveConfig')->name('saveConfig');
-
-                Route::get('/packageConfig', 'SettingController@packageConfig')->name('packageConfig');
-                Route::post('/savePackage', 'SettingController@savePackage')->name('savePackage');
-
-            });
-
-        });
-    });
-});

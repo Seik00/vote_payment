73460ab86e5d5e81dfb2b170c2bddd243fdc2069
WChienMing <woochienming@gmail.com>
2023-06-22 16:50:55 +0800

    update admin table

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,76 +1,131 @@
 @extends('admin.layouts.header')
 @section('subtitle')
-    <li class="breadcrumb-item active ">Welcome back to your <span class="f-w-600 color-light-job">Dashboard</span>!</li>
+    <li class="breadcrumb-item active ">欢迎回到<span class="f-w-600 color-light-job">仪表盘</span>！</li>
 @stop
 @section('content')
 <div class="row"> 
-<div class="col-12">
-            <div class="card">
-                <div class="card-body">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
                 <h4 class="header-title mt-0">Payment Details</h4>
                 <br>
                 <div class="table-responsive dash-social">
-                    <table id="usersdatatable" class="table">
+                    <table id="usersdatatable" class="table " style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                         <thead class="thead-light">
-                        <tr>
-                            <th>ID</th>
-                            <th>USDT Address</th>
-                            <th>Username</th>
-                            <th>Phone</th> 
-                            <th>Payee ID</th>     
-                            <th>Email</th>     
-                            <th>Currecy Type</th>     
-                            <th>Order No</th>     
-                            <th>Bank Account</th> 
-                            <th>Amount</th>      
-                            <th>USDT Amount</th>              
-                            <th>Status</th> 
-                            <th>Date</th>                                             
-                        </tr><!--end tr-->
-                        </thead>
-
-                        <tbody>
-                        @foreach ($orders as $order)
                             <tr>
+                                <th></th>
+                                <th>ID</th>
+                                <th>USDT Address</th>
+                                <th>Username</th>
+                                <th>Phone</th> 
+                                <th>Payee ID</th> 
+
+                                <th style="display:none;">Email</th>     
+                                <th style="display:none;">Currecy Type</th>     
+                                <th style="display:none;">Order No</th>     
+                                <th style="display:none;">Bank Account</th> 
+                                <th style="display:none;">Amount</th>      
+                                <th style="display:none;">USDT Amount</th>              
+                                <th style="display:none;">Status</th> 
+                                <th style="display:none;">Date</th> 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($orders as $order)
+                            <tr>
+                                <td class="details-control"></td>
                                 <td>{{ $order->id }}</td>
                                 <td>{{ $order->usdt_address }}</td>
                                 <td>{{ $order->username }}</td>
                                 <td>{{ $order->phone }}</td>
                                 <td>{{ $order->payee_id }}</td>
-                                <td>{{ $order->email }}</td>
-                                <td>{{ $order->currency }}</td>
-                                <td>{{ $order->order_no }}</td>
-                                <td>{{ $order->bank_account_number }}</td>
-                                <td>{{ $order->amount }}</td>
-                                <td>{{ $order->usdt_amount }}</td>
-                                @if ($order->payment_status == 0)
-                                    <td>待办</td>
-                                @elseif ($order->payment_status == 1)
-                                    <td>取消支付</td>
-                                @elseif ($order->payment_status == 66)
-                                    <td>成功支付</td>
-                                @elseif ($order->payment_status == 999)
-                                    <td>验签失败</td>
-                                @endif
-                                <td>{{ $order->created_at }}</td>
+                                
+                                <td style="display:none;">{{ $order->email }}</td>
+                                <td style="display:none;">{{ $order->currency }}</td>
+                                <td style="display:none;">{{ $order->order_no }}</td>
+                                <td style="display:none;">{{ $order->bank_account_number }}</td>
+                                <td style="display:none;">{{ $order->amount }}</td>
+                                <td style="display:none;">{{ $order->usdt_amount }}</td>
+                                <td style="display:none;">{{ $order->payment_status }}</td>
+                                <td style="display:none;">{{ $order->created_at }}</td>
                             </tr>
-                        @endforeach                            
+                            @endforeach
                         </tbody>
-                    </table>                    
-                </div>                                         
-            </div><!--end card-body--> 
-        </div><!--end card--> 
-    </div> <!--end col-->      
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @stop
 
 @section('script')
     <script>
         $(document).ready(function() {
-        $('#usersdatatable').DataTable({
-            order: [[0, 'desc']]
-        });
+            var table = $('#usersdatatable').DataTable({
+                order: [[1, 'desc']]
+            });
+
+            // Add event listener for opening and closing details
+            $('#usersdatatable tbody').on('click', 'td.details-control', function () {
+                var tr = $(this).closest('tr');
+                var row = table.row(tr);
+
+                if (row.child.isShown()) {
+                    // This row is already open - close it
+                    row.child.hide();
+                    tr.removeClass('shown');
+                } else {
+                    // Open this row
+                    var rowData = table.row(tr).data();
+                    var email = rowData[6];
+                    var currency = rowData[7];
+                    var orderNo = rowData[8];
+                    var bankAccount = rowData[9];
+                    var amount = rowData[10];
+                    var usdtAmount = rowData[11];
+                    var status = rowData[12];
+                    var date = rowData[13];
+
+                    var html = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+                        '<tr>'+
+                            '<th>Email:</th>'+
+                            '<td>'+email+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>Currecy Type:</th>'+
+                            '<td>'+currency+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>Order No:</th>'+
+                            '<td>'+orderNo+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>Bank Account:</th>'+
+                            '<td>'+bankAccount+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>Amount:</th>'+
+                            '<td>'+amount+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>USDT Amount:</th>'+
+                            '<td>'+usdtAmount+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>Status:</th>'+
+                            '<td>'+status+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<th>Date:</th>'+
+                            '<td>'+date+'</td>'+
+                        '</tr>'+
+                    '</table>';
+
+                    row.child(html).show();
+                    tr.addClass('shown');
+                }
+            });
         });
     </script>
 @stop
-
--- resources/views/admin/home/setting.blade.php
+++ resources/views/admin/home/setting.blade.php
@@ -3,30 +3,30 @@
     <li class="breadcrumb-item active ">Welcome back to your <span class="f-w-600 color-light-job">Dashboard</span>!</li>
 @stop
 @section('content')
-<div class="row"> 
-<div class="col-12">
-            <div class="card">
-                <div class="card-body">
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
                 @if (session('message'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ session('message') }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('message') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
                 @endif
                 <form method="post" action="/admin/do_setting">
-                @csrf
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                        <label>Exchange Rate</label>
-                        <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
-                            <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" name="rate" value="{{ $rate->key_value }}"  required>
-                        </div>
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label>Exchange Rate</label>
+                                <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                                    <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" name="rate" value="{{ $rate->key_value }}" required>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
                     <div class="col-md-12">
                         <!-- <div class="form-group"> -->
                         <div class="input-group date" id="reservationdatetime" data-target-input="nearest" style="display: block;">
@@ -34,10 +34,13 @@
                         </div>
                         <!-- </div> -->
                     </div>
-                </form>                                      
-            </div><!--end card-body--> 
-        </div><!--end card--> 
-    </div> <!--end col-->      
+                </form>
+            </div>
+            <!--end card-body-->
+        </div>
+        <!--end card-->
+    </div>
+    <!--end col-->
 </div>
 @stop
 
